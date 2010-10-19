4c4487de691cd199f9288d173f47e1cae0f8c38d
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2010-10-20 01:28:26 +1030

    Added tag 1.0.1 for changeset c4d5651db316

---
+++ CityLizard/XHtml/Example/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CityLizard.XHtml.Example
+{
+    using H = www_w3_org._1999.xhtml;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Generator : H.X
+        {
+            public static T.html Generate()
+            {
+                return
+                    html
+                        [head
+                            [title["Title of the page"]]
+                            [meta_(
+                                content: "text/html;charset=UTF-8",
+                                http_equiv: "Content-Type")
+                            ]
+                            [link_(href: "css/style.css", rel: "stylesheet", type: "text/css")]
+                            // [script_(type: "text/javascript", src: "/JavaScript/jquery-1.4.2.min.js")]
+                        ]
+                        [body
+                            [div
+                                [h1["Test Form to Test"]]
+                                [form_(action: "post", id: "Form1")
+                                    [div
+                                        [label["Parameter"]]
+                                        [input_(type: "text", value: "Enter value")]
+                                        [input_(type: "submit", value: "Submit!")]
+                                    ]
+                                ]
+                                [div
+                                    [p["Textual description of the footer"]]
+                                    [a_(href: "http://google.com/")
+                                        [span["You can find us here"]]
+                                    ]
+                                    [div["Another nested container"]]
+                                ]
+                            ]
+                        ];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var h = Generator.Generate();
+            var s = h.ToString();
+            this.webBrowser1.DocumentText = s;
+        }
+    }
+}
---
+++ CityLizard/Xml/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace CityLizard.Xml
+{
+    using T = System.Text;
+    using S = System;
+    using C = System.Collections.Generic;
+
+    using System.Linq;
+    using Extension;
+
+    public abstract class Node : INode
+    {
+        public abstract void ToStringBuilder(
+            T.StringBuilder builder, string parentNamespace);
+
+        public override string ToString()
+        {
+            var builder = new T.StringBuilder();
+            builder.AppendNode(this, null);
+            return builder.ToString();
+        }
+    }
+}

6db622930f843c7e3b791e5e61485e99b9c9818a
sergey_shandar <sergey_shandar@localhost>
2010-10-30 11:20:01 +1030

    AssemblyInfo

--- CityLizard/Build/Program.cs
+++ CityLizard/Build/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace CityLizard.Build
 {
-    using D = System.Diagnostics;
     using S = System;
     using IO = System.IO;
+    using CD = System.CodeDom;
+    using R = System.Reflection;
+    using CS = Microsoft.CSharp;
 
     using System.Linq;
+    using CodeDom.Extension;
 
     class Program
     {
@@ -15,11 +18,35 @@
                 (s.Branch == "default" ? "0.0.0": s.Branch) + 
                 "." + 
                 s.Parent.RevisionNumber;
-            var r = Hg.Hg.Manifest();
+            var r = Hg.Hg.Locate(true);
             foreach (var i in r.Where(x => x.EndsWith(".csproj")))
             {
                 S.Console.WriteLine("Project: " + i);
                 var d = IO.Path.GetDirectoryName(i);
+                var f = IO.Path.GetFileNameWithoutExtension(i);
+                var u = new CD.CodeCompileUnit();
+                u.AssemblyCustomAttributes.
+                    AddDeclarationString<R.AssemblyVersionAttribute>(version);
+                u.AssemblyCustomAttributes.
+                    AddDeclarationString<R.AssemblyFileVersionAttribute>(
+                        version);
+                u.AssemblyCustomAttributes.
+                    AddDeclarationString<R.AssemblyTitleAttribute>(f);
+                u.AssemblyCustomAttributes.
+                    AddDeclarationString<R.AssemblyCompanyAttribute>(
+                        "CityLizard");
+                u.AssemblyCustomAttributes.
+                    AddDeclarationString<R.AssemblyProductAttribute>(f);
+                u.AssemblyCustomAttributes.
+                    AddDeclarationString<R.AssemblyCopyrightAttribute>(
+                        "Copyright © CityLizard 2010");
+                var p = new CS.CSharpCodeProvider();
+                using(var w = new IO.StreamWriter(IO.Path.Combine(
+                    d, "Properties\\AssemblyInfo.cs")))
+                {
+                    p.GenerateCodeFromCompileUnit(
+                        u, w, new CD.Compiler.CodeGeneratorOptions());
+                }
             }
         }
     }

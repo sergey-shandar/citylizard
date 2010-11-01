f75f32441c25f8f6cb498e8de036142d4417e386
sergey_shandar <sergey_shandar@localhost>
2010-11-02 00:33:59 +1030

    powershell: assemblyinfo files.

--- CityLizard/Hg/Hg.cs
+++ CityLizard/Hg/Hg.cs
@@ -1,6 +1,12 @@
 ﻿namespace CityLizard.Hg
 {
     using D = System.Diagnostics;
+    using IO = System.IO;
+    using CD = System.CodeDom;
+    using R = System.Reflection;
+    using CS = Microsoft.CSharp;
+
+    using CityLizard.CodeDom.Extension;
 
     public static class Hg
     {
@@ -64,6 +70,36 @@
                 return (this.Branch == "default" ? "0.0.0" : this.Branch) +
                     "." +
                     this.Parent.RevisionNumber;
+            }
+
+            private static void Add<T>(CD.CodeCompileUnit u, string v)
+            {
+                u.AssemblyCustomAttributes.AddDeclarationString<T>(v);
+            }
+
+            public void CreateAssemblyInfo(string company, string i)
+            {
+                var d = IO.Path.GetDirectoryName(i);
+                var f = IO.Path.GetFileNameWithoutExtension(i);
+                var u = new CD.CodeCompileUnit();
+                var version = this.Version();
+                Add<R.AssemblyVersionAttribute>(u, version);
+                Add<R.AssemblyVersionAttribute>(u, version);
+                Add<R.AssemblyFileVersionAttribute>(u, version);
+                Add<R.AssemblyTitleAttribute>(u, f);
+                Add<R.AssemblyCompanyAttribute>(u, company);
+                Add<R.AssemblyProductAttribute>(u, f);
+                Add<R.AssemblyCopyrightAttribute>(
+                    u, "Copyright © " + company + " 2010");
+                var p = new CS.CSharpCodeProvider();
+                var dir = IO.Path.Combine(d, "Properties");
+                IO.Directory.CreateDirectory(dir);
+                var a = IO.Path.Combine(dir, "AssemblyInfo.cs");
+                using (var w = new IO.StreamWriter(a))
+                {
+                    p.GenerateCodeFromCompileUnit(
+                        u, w, new CD.Compiler.CodeGeneratorOptions());
+                }
             }
         }
 
--- CityLizard/build.ps1
+++ CityLizard/build.ps1
@@ -5,18 +5,40 @@
 #  2. Copy powershell_ise.exe.config C:\Windows\System32\WindowsPowerShell\v1.0\
 #
 
+$company = "CityLizard"
+
 $dir = Split-Path -parent $MyInvocation.MyCommand.Definition
-$path = Join-Path $dir Hg\Hg.cs
+"dir: " + $dir
+[IO.Directory]::SetCurrentDirectory($dir)
 
-"path: " + $path
+$params = 
+@{
+    Path =
+        "CodeDom\Extension\AttributeDeclarationCollection.cs",
+        "CodeDom\Extension\NamespaceCollectionExtension.cs",
+        "Hg\Hg.cs"
+    ReferencedAssemblies = "System.Core"
+}
 
-[System.IO.Directory]::SetCurrentDirectory($dir)
+Add-Type @params
 
-Add-Type -Path $path
+$root = [CityLizard.Hg.Hg]::Root()
+"root: " + $root
 
-"root: " + [CityLizard.Hg.Hg]::Root()
+$summary = [CityLizard.Hg.Hg]::Summary()
+"version: " + $summary.Version()
 
-$version = [CityLizard.Hg.Hg]::Summary()
-"version: " + $version
-$locate = [CityLizard.Hg.Hg]::Locate()
-$locate
+""
+"projects:"
+foreach($f in [CityLizard.Hg.Hg]::Locate())
+{
+    if([IO.Path]::GetExtension($f) -eq ".csproj")
+    {
+        "    " + $f
+        $path = Join-Path $root $f
+        $summary.CreateAssemblyInfo($company, $path)
+    }
+}
+
+# $locate = [CityLizard.Hg.Hg]::Locate()
+# $locate

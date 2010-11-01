a194ed1ceea41423f1663250f541687e53176661
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2010-11-01 22:37:09 +1030

    powershell build is in progress...

--- CityLizard/build.ps1
+++ CityLizard/build.ps1
@@ -1,9 +1,22 @@
 #
-# Before running the script, run:
-# > Set-ExecutionPolicy RemoteSigned
+# Before running the script, 
+#  1. Execute in PowerShell:
+#     Set-ExecutionPolicy RemoteSigned
+#  2. Copy powershell_ise.exe.config C:\Windows\System32\WindowsPowerShell\v1.0\
 #
-Add-Type -Path .\Hg\Hg.cs
-$version = [CityLizard.Hg.Hg]::Summary().Version()
+
+$dir = Split-Path -parent $MyInvocation.MyCommand.Definition
+$path = Join-Path $dir Hg\Hg.cs
+
+"path: " + $path
+
+[System.IO.Directory]::SetCurrentDirectory($dir)
+
+Add-Type -Path $path
+
+"root: " + [CityLizard.Hg.Hg]::Root()
+
+$version = [CityLizard.Hg.Hg]::Summary()
 "version: " + $version
 $locate = [CityLizard.Hg.Hg]::Locate()
 $locate

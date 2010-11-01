2c1750bc53dff55b820f65a3ec1e452fcdc8ef01
sergey_shandar <sergey_shandar@localhost>
2010-11-01 21:57:44 +1030

    minor

--- CityLizard/build.ps1
+++ CityLizard/build.ps1
@@ -1,7 +1,9 @@
+#
+# Before running the script, run:
+# > Set-ExecutionPolicy RemoteSigned
+#
 Add-Type -Path .\Hg\Hg.cs
 $version = [CityLizard.Hg.Hg]::Summary().Version()
 "version: " + $version
-#$locate = [CityLizard.Hg.Hg]::Locate()
-#"Files:"
-#$locate
-#"."
+$locate = [CityLizard.Hg.Hg]::Locate()
+$locate

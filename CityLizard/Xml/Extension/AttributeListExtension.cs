316d849f40c17717f17b51d8caeed5e8843b974c
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2010-11-05 07:17:50 +1030

    comments.

--- CityLizard/Xml/Extension/AttributeListExtension.cs
+++ CityLizard/Xml/Extension/AttributeListExtension.cs
@@ -2,12 +2,21 @@
 {
     using C = System.Collections.Generic;
 
+    /// <summary>
+    /// Extension methods for XML attributes.
+    /// </summary>
     public static class AttributeListExtension
     {
+        /// <summary>
+        /// Adds an attribute to the list.
+        /// </summary>
+        /// <param name="this_">The list of attributes.</param>
+        /// <param name="name">The attribute name.</param>
+        /// <param name="value">The attribute value.</param>
         public static void Add(
-            this C.IList<IAttribute> x, string name, string value)
+            this C.IList<IAttribute> this_, string name, string value)
         {
-            x.Add(new Attribute(null, name, value));
+            this_.Add(new Attribute(null, name, value));
         }
     }
 }

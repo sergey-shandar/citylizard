a13b28c216df349f1ad59e9930b3fa218c41818c
sergey_shandar <sergey_shandar@localhost>
2010-11-11 23:36:11 +1030

    preparation.

---
+++ CityLizard/CodeDom/CodeDom.cs
@@ -0,0 +1,39 @@
+﻿namespace CityLizard.CodeDom
+{
+    using D = System.CodeDom;
+
+    public partial class CodeDom
+    {
+        public static class T
+        {
+            public class Unit : D.CodeCompileUnit
+            {
+                public Unit this[Namespace Namespace]
+                {
+                    get
+                    {
+                        this.Namespaces.Add(Namespace);
+                        return this;
+                    }
+                }
+            }
+
+            public class Namespace : D.CodeNamespace
+            {
+                public Namespace(string Name): base(Name)
+                {
+                }
+            }
+        }
+
+        public T.Unit Unit()
+        {
+            return new T.Unit();
+        }
+
+        public T.Namespace Namespace(string Name)
+        {
+            return new T.Namespace(Name);
+        }
+    }
+}

0af2818831c9b0f6d35d7f282281a92b73b6f8e7
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2010-11-14 23:49:46 +1030

    Schema Compiler (WiP)

---
+++ CityLizard/NUnit/CompilerTest.cs
@@ -0,0 +1,32 @@
+﻿namespace CityLizard.NUnit
+{
+    using N = global::NUnit.Framework;
+    using IO = System.IO;
+    using CS = Microsoft.CSharp;
+    using D = System.CodeDom;
+
+    using S = CityLizard.Xml.Schema;
+
+    [N.TestFixture]
+    public static class CompilerTest
+    {
+        [N.Test]
+        public static void Load()
+        {
+            var u = new S.Compiler().Load(
+                "../../../../www.w3.org/MarkUp/SCHEMA/xhtml11.xsd");
+            //
+            var t = new IO.StringWriter();
+            new CS.CSharpCodeProvider().GenerateCodeFromCompileUnit(
+                u, t, new D.Compiler.CodeGeneratorOptions());
+            var code = t.ToString();
+            //
+            using (var w =
+                new IO.StreamWriter(
+                    "../../../XHtml/xhtml11.xsd.cs"))
+            {
+                w.Write(code);
+            }
+        }
+    }
+}

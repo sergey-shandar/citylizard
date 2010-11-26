5405bc736dd6814ad990be10bd26c3bf292b6527
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2010-11-26 23:26:53 +1030

    1.2.2

---
+++ CityLizard/Xml/Linked/Element/Type.cs
@@ -0,0 +1,28 @@
+﻿namespace CityLizard.Xml.Linked.Element
+{
+    /// <summary>
+    /// The element type.
+    /// </summary>
+    public enum Type
+    {
+        /// <summary>
+        /// Simple type (simpleType).
+        /// </summary>
+        Simple,
+
+        /// <summary>
+        /// Complex type (complexType), empty.
+        /// </summary>
+        Empty,
+
+        /// <summary>
+        /// Complex type (complexType mixed="false"), not empty, not mixed.
+        /// </summary>
+        NotMixed,
+
+        /// <summary>
+        /// Complex type (complextType mixed="true"), not empty, mixed.
+        /// </summary>
+        Mixed,
+    }
+}

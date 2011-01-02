c30497ef3b545ac0d2a27e6c0aca8c5d5c6ab66a
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2011-01-03 00:05:18 +1030

    minor.

--- CityLizard/Fsm/Dfa.cs
+++ CityLizard/Fsm/Dfa.cs
@@ -5,35 +5,6 @@
 
     using System.Linq;
 
-    public class Name : C.HashSet<int>, S.IEquatable<Name>
-    {
-        public override bool Equals(object obj)
-        {
-            return this.Equals(obj as Name);
-        }
-
-        public bool Equals(Name other)
-        {
-            return base.SetEquals(other);
-        }
-
-        /// <summary>
-        /// Note: the function implementation has really bad distribution. 
-        /// However it works.
-        /// Requirements: { 0, 1 }.GetHachCode() == { 1, 0 }.GetHashCode().
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            uint hash = 0;
-            foreach (var i in this)
-            {
-                hash ^= (uint)i;
-            }
-            return (int)hash;
-        }
-    }
-
     /// <summary>
     /// Deterministic finite automaton.
     /// http://en.wikipedia.org/wiki/Deterministic_finite_automaton

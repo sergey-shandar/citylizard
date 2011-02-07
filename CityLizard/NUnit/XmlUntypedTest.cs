f3aeb7976aac30aaa9c72489959e32dcebc7a61f
sergey_shandar <sergey_shandar@localhost>
2011-02-08 01:34:52 +1030

    Untyped XML.

--- CityLizard/NUnit/XmlUntypedTest.cs
+++ CityLizard/NUnit/XmlUntypedTest.cs
@@ -16,14 +16,46 @@
                 return
                     X("html")
                         [X("head")
-                            [X("title")["Hello world!"]]
+                            [X("title")["Title of the page"]]
+                            [E("meta",
+                                A("content", "text/html;charset=UTF-8"),
+                                A("http_equiv", "Content-Type"))
+                            ]
+                            [E("link", 
+                                A("href", "css/style.css"), 
+                                A("rel", "stylesheet"), 
+                                A("type", "text/css"))
+                            ]
+                            [X("script", 
+                                A("type", "text/javascript"), 
+                                A("src", "/JavaScript/jquery-1.4.2.min.js"))
+                            ]
                         ]
                         [X("body")
                             [X("div")
-                                ["Hello world!"]
-                                [E("br")]
-                                [X("a", A("href", "http://google.com/"))
-                                    ["http://google.com/"]
+                                [X("h1")["Test Form to Test"]]
+                                [X("form", 
+                                    A("action", "post"), A("id", "Form1"))
+                                    [X("div")
+                                        [X("label")["Parameter"]]
+                                        [E("input", 
+                                            A("type", "text"), 
+                                            A("value", "Enter value"))
+                                        ]
+                                        [E("input", 
+                                            A("type", "submit"), 
+                                            A("value", "Submit!"))
+                                        ]
+                                    ]
+                                ]
+                                [X("div")
+                                    [X("p")
+                                        ["Textual description of the footer"]
+                                    ]
+                                    [X("a", A("href", "http://google.com/"))
+                                        [X("span")["You can find us here"]]
+                                    ]
+                                    [X("div")["Another nested container"]]
                                 ]
                             ]
                         ];

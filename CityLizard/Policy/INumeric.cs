9c84fd4d7e19ac0384cc7287985070dae79f49df
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2011-02-07 19:59:15 +1030

    Policies

--- CityLizard/Policy/INumeric.cs
+++ CityLizard/Policy/INumeric.cs
@@ -2,7 +2,11 @@
 {
     public interface INumeric<T>
     {
-        T Zero();
+        T _0();
+        T _1();
         T Add(T a, T b);
+        T Subtract(T a, T b);
+        T Multiply(T a, T b);
+        T Divide(T a, T b);
     }
 }

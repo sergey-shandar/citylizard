fdfc5b6314ee5277dfe705620746911f8b5a8a8c
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2011-04-14 04:47:36 +0930

    Tree. WIP.

---
+++ CityLizard/Tree/RedBlack.cs
@@ -0,0 +1,13 @@
+﻿namespace CityLizard.Tree
+{
+    class RedBlack<T>
+    {
+        private struct Data
+        {
+            public bool Red;
+            public T Value;
+        }
+
+        private Base<Data> Base;
+    }
+}

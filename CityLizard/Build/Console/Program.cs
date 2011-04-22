6f958f24e9ca8de3f08ac76cd6afcb1c9d75354a
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2011-04-22 15:10:10 +0930

    Build.Console

---
+++ CityLizard/Build/Console/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CityLizard.Build.Console
+{
+    using IO = System.IO;
+    using R = System.Reflection;
+    using S = System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set current directory to ".../CityLizard/"
+            var dir = IO.Path.GetDirectoryName(R.Assembly.GetExecutingAssembly().Location);
+            while (IO.Path.GetFileName(dir) != "CityLizard")
+            {
+                dir = IO.Path.GetDirectoryName(dir);
+            }
+            IO.Directory.SetCurrentDirectory(dir);
+            S.Console.WriteLine(IO.Directory.GetCurrentDirectory());
+            //
+            var root = Hg.Hg.Root();
+            var summary = Hg.Hg.Summary();
+            var version = Build.Version(summary);
+        }
+    }
+}

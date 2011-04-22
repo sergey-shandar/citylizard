108e03494bd89b4a8e2ba40c6584e2762eeb18a7
sergey_shandar <sergey_shandar@localhost>
2011-04-22 23:49:07 +0930

    Build.Console.

--- CityLizard/Build/Console/Program.cs
+++ CityLizard/Build/Console/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CityLizard.Build.Console
+﻿using System.Linq;
+
+namespace CityLizard.Build.Console
 {
     using IO = System.IO;
     using R = System.Reflection;
@@ -6,6 +8,8 @@
 
     class Program
     {
+        const string Company = "CityLizard";
+
         static void Main(string[] args)
         {
             // Set current directory to ".../CityLizard/"
@@ -20,6 +24,21 @@
             var root = Hg.Hg.Root();
             var summary = Hg.Hg.Summary();
             var version = Build.Version(summary);
+            //
+            foreach (
+                var file in 
+                Hg.Hg.Locate().Where(
+                    file => IO.Path.GetExtension(file) == ".vcproj"))
+            {
+                Build.CreateAssemblyInfo(
+                    summary, Company, IO.Path.Combine(root, file));
+            }
+            //
+            Policy.Build.Base.Run();
+            //
+            Build.BuildSolution(
+                Hg.Hg.Locate().First(
+                    file => IO.Path.GetFileName(file) == "CityLizard.sln"));
         }
     }
 }

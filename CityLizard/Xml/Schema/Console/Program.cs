6e9a020326a7bda8bf82a1da685c928bba7f218d
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2011-04-24 23:36:42 +0930

    www

--- CityLizard/Xml/Schema/Console/Program.cs
+++ CityLizard/Xml/Schema/Console/Program.cs
@@ -3,21 +3,29 @@
     using IO = System.IO;
     using CS = Microsoft.CSharp;
     using D = System.CodeDom;
+    using S = System;
 
     class Program
     {
         static void Main(string[] args)
         {
-            var u = new Compiler().Load(args[0]);
-            //
-            var t = new IO.StringWriter();
-            new CS.CSharpCodeProvider().GenerateCodeFromCompileUnit(
-                u, t, new D.Compiler.CodeGeneratorOptions());
-            var code = t.ToString();
-            //
-            using (var w = new IO.StreamWriter(args[1]))
+            try
             {
-                w.Write(code);
+                var u = new Compiler().Load(args[0]);
+                //
+                var t = new IO.StringWriter();
+                new CS.CSharpCodeProvider().GenerateCodeFromCompileUnit(
+                    u, t, new D.Compiler.CodeGeneratorOptions());
+                var code = t.ToString();
+                //
+                using (var w = new IO.StreamWriter(args[1]))
+                {
+                    w.Write(code);
+                }
+            }
+            catch (S.Exception e)
+            {
+                S.Console.WriteLine("error: " + e.Message);
             }
         }
     }

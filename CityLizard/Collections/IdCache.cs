f70a97c8b7da05f4640ff84b874a9bd785a30330
sergey_shandar <sergey_shandar@localhost>
2011-09-01 02:31:30 +0930

    Collections.

---
+++ CityLizard/Collections/IdCache.cs
@@ -0,0 +1,21 @@
+﻿namespace CityLizard.Collections
+{
+    public abstract class IdCache<Key, Data> : Cache<Key, Data>
+    {
+        private int Value;
+
+        private int GetNew()
+        {
+            var value = this.Value;
+            ++this.Value;
+            return value;
+        }
+
+        protected abstract Data Create(Key key, int id);
+
+        protected sealed override Data Create(Key key)
+        {
+            return this.Create(key, this.GetNew());
+        }
+    }
+}

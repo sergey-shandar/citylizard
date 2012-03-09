9601ee6934dfdcb76a8de4b74def6fa94139d6d8
ser 000 <ser.000@windows8>
2012-03-09 21:19:05 +1030

    C++ PInvoke test

--- mono/CityLizard/PInvoke/Console/Main.cs
+++ mono/CityLizard/PInvoke/Console/Main.cs
@@ -2,12 +2,18 @@ using System;
 
 namespace CityLizard.PInvoke.Console
 {
+    using IO = System.IO;
+
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
 			var builder = new CppBuilder();
-            System.Console.Write(builder.Build(typeof(Test.MyClass).Assembly));
+            var sb = builder.Build(typeof(Test.MyClass).Assembly);
+            using (var outfile = new IO.StreamWriter("../../../../citylizard_pinvoke_test_cpp/interface.hpp"))
+            {
+                outfile.Write(sb.ToString());
+            }
 		}
 	}
 }

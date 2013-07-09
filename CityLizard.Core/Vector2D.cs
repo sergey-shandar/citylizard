cc51d4de886d9c65877207d0ec815c35419792b3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2013-07-09 07:58:39 -0700

    policy

---
+++ CityLizard.Core/Vector2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CityLizard
+{
+    public struct Vector2D<I, T>
+        where T : struct, IComparable<T>
+        where I: struct, Policy.INumeric<T>
+    {
+        public T X;
+        public T Y;
+    }
+}

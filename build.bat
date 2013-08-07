0f19b17c1e30777ac7483a4c23c0b979ebfe2823
sergey <sergey@localhost>
2013-08-07 12:55:12 -0700

    nuspec

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 setlocal
-set PATH=%PATH%C:\Windows\Microsoft.NET\Framework\v4.0.30319;
+set PATH=%PATH%C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\programs;
 msbuild desktop.sln
 msbuild web.sln
 msbuild windows8.sln
 msbuild wp.sln
 msbuild psm.sln
+nuget
 endlocal

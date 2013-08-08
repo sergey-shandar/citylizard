9337b8635cc538629fb31162227d543f677d2ddf
sergey <sergey@localhost>
2013-08-08 12:47:14 -0700

    better versioning.

--- CityLizard.AssemblyInfo.cs
+++ CityLizard.AssemblyInfo.cs
@@ -1,22 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyCompany("CityLizard")]
-[assembly: AssemblyCopyright("Copyright © CityLizard 2013")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.3.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
+using System.Reflection; 
+[assembly: AssemblyCompany("CityLizard")] 
+[assembly: AssemblyCopyright("Copyright � CityLizard 2013")] 
+[assembly: AssemblyFileVersion("2.1.4.0")] 

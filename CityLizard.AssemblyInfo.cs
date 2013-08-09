8599ab1e73ab28af4a9bb4c2d7b36a4a3bc30a51
Sergey Shandar <sergey.shandar@gmail.com>
2013-08-09 16:33:27 -0700

    build.sh

--- CityLizard.AssemblyInfo.cs
+++ CityLizard.AssemblyInfo.cs
@@ -1,5 +1,5 @@
-using System.Reflection; 
-[assembly: AssemblyCompany("CityLizard")] 
-[assembly: AssemblyCopyright("Copyright © CityLizard 2013")] 
-[assembly: AssemblyVersion("2.1.7.0")] 
-[assembly: AssemblyFileVersion("2.1.7.0")] 
+using System.Reflection;
+[assembly: AssemblyCompany("CityLizard")]
+[assembly: AssemblyCopyright("Copyright Â© CityLizard 2013")]
+[assembly: AssemblyVersion("2.1.7.0")]
+[assembly: AssemblyFileVersion("2.1.7.0")]

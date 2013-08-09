1cb88433f967534f5854d600a29da131f35d5f3c
sergey <sergey@localhost>
2013-08-09 14:22:12 -0700

    2.1.7.0

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 setlocal
 set PATH=%PATH%C:\Windows\Microsoft.NET\Framework\v4.0.30319;C:\programs;C:\Program Files\7-Zip
 
-set VERSION=2.1.6.0
+set VERSION=2.1.7.0
 
 echo using System.Reflection; > CityLizard.AssemblyInfo.cs
 echo [assembly: AssemblyCompany("CityLizard")] >> CityLizard.AssemblyInfo.cs
@@ -13,8 +13,22 @@ msbuild desktop.sln
 msbuild web.sln
 msbuild windows8.sln
 msbuild wp.sln
-msbuild psm.sln
+msbuild psm.sln /p:Configuration=Release
 
 nuget pack CityLizard.nuspec -Version %VERSION%
+
+rmdir /S /Q lib
+
+xcopy CityLizard.Meta\bin\net40-client\*.dll lib\net40-client\
+xcopy CityLizard.Meta\bin\net35-client\*.dll lib\net35-client\
+xcopy CityLizard.Fsm\bin\sl4\CityLizard.*.dll lib\sl4\
+xcopy CityLizard.Fsm\bin\sl5\CityLizard.*.dll lib\sl5\
+xcopy CityLizard.Fsm\bin\windows8\*.dll lib\netcore45\
+xcopy CityLizard.Fsm\bin\wp8\*.dll lib\wp8\
+xcopy CityLizard.Fsm\bin\Release.psm\*.dll lib\psm\
+xcopy CityLizard.Core\bin\wp71\*.dll lib\sl4-wp71\ 
+
+del CityLizard.%VERSION%.zip
+7z a CityLizard.%VERSION%.zip lib
 
 endlocal

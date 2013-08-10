71bf54d5b4ba60ed6c07104b101cbecb7d339a20
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2013-08-09 23:25:40 -0700

    version.txt

--- CityLizard.AssemblyInfo.cs
+++ CityLizard.AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection; 
 [assembly: AssemblyCompany("CityLizard")] 
 [assembly: AssemblyCopyright("Copyright © CityLizard 2013")] 
-[assembly: AssemblyVersion("2.1.8.0")] 
-[assembly: AssemblyFileVersion("2.1.8.0")] 
+[assembly: AssemblyVersion("2.1.0.8")] 
+[assembly: AssemblyFileVersion("2.1.0.8")] 

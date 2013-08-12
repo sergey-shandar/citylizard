66acffd4f0e3851e2f82926c49671aaeeb36c761
sergey <sergey@localhost>
2013-08-11 20:20:36 -0700

    2.1.9.0

--- CityLizard.AssemblyInfo.cs
+++ CityLizard.AssemblyInfo.cs
@@ -1,5 +1,5 @@
-using System.Reflection;
-[assembly: AssemblyCompany("CityLizard")]
-[assembly: AssemblyCopyright("Copyright © CityLizard 2013")]
-[assembly: AssemblyVersion("2.1.9.0")]
-[assembly: AssemblyFileVersion("2.1.9.0")]
+using System.Reflection; 
+[assembly: AssemblyCompany("CityLizard")] 
+[assembly: AssemblyCopyright("Copyright © CityLizard 2013")] 
+[assembly: AssemblyVersion("2.1.9.0")] 
+[assembly: AssemblyFileVersion("2.1.9.0")] 

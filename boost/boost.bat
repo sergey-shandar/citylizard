2c7cdd386b7d1bec4d0b618b64d6a2b392551f2f
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2013-08-18 11:38:09 -0700

    console

--- boost/boost.bat
+++ boost/boost.bat
@@ -7,13 +7,7 @@ bjam msvc architecture=x86 stage --stagedir=stage_x86
 endlocal
 
 setlocal
-rem call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /x64
 bjam msvc architecture=x86 address-model=64 stage --stagedir=stage_x86_64
 endlocal
-
-rem setlocal
-rem call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.Cmd" /ia64
-rem bjam msvc architecture=ia64 stage --stagedir=stage_ia64
-rem endlocal
 
 endlocal

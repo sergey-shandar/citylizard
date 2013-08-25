209fd44602da8559084a308321889b3a5b31a7e4
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2013-08-25 10:27:48 -0700

    folder change

--- check.bat
+++ check.bat
@@ -21,6 +21,12 @@ sn -T netcore45\CityLizard.Fsm\bin\Release\CityLizard.Fsm.dll
 sn -T psm\CityLizard.Core\bin\Release\CityLizard.Core.dll
 sn -T psm\CityLizard.Fsm\bin\Release\CityLizard.Fsm.dll
 
+sn -T monoandroid\CityLizard.Core\bin\Release\CityLizard.Core.dll
+sn -T monoandroid\CityLizard.Fsm\bin\Release\CityLizard.Fsm.dll
+
+sn -T monotouch\CityLizard.Fsm\bin\Release\CityLizard.Core.dll
+sn -T monotouch\CityLizard.Fsm\bin\Release\CityLizard.Fsm.dll
+
 sn -T wp8\CityLizard.Core\bin\Release\CityLizard.Core.dll
 sn -T wp8\CityLizard.Fsm\bin\Release\CityLizard.Fsm.dll
 

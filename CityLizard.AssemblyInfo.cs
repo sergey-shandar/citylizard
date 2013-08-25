65c11774e88db190da56bf69c01c8d4570374079
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2013-08-25 00:59:10 -0700

    structure 2

--- CityLizard.AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection; 
-[assembly: AssemblyCompany("CityLizard")] 
-[assembly: AssemblyCopyright("Copyright © CityLizard 2013")] 
-[assembly: AssemblyVersion("2.1.10.0")] 
-[assembly: AssemblyFileVersion("2.1.10.0")] 

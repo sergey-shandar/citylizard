bd7fcf13469a2f74f684d33f5969771541a226be
Sergey Shandar <serge.shandar@gmail.com>
2013-08-30 20:02:05 -0700

    vector

--- cpp/citylizard/clr/array.h
+++ cpp/citylizard/clr/array.h
@@ -15,8 +15,7 @@ namespace clr
     vector<Native> managed_cast(array<Managed>^ managed, tag<vector<Native>>)
     {
         auto size = managed == nullptr ? 0: managed->Length;
-        NativeVector result(size);
-        typedef typename remove_reference<decltype(result[0])>::type Native;
+        vector<Native> result(size);
         for(auto i = 0; i < size; ++i)
         {
             result[i] = managed_cast(managed[i], tag<Native>());
@@ -24,5 +23,14 @@ namespace clr
         return result;
     }
 
+    template<class Native, class Managed>
+    void copy(vector<Native> const &from, array<Managed>^ to)
+    {
+        auto size = static_cast<int>(from.size());
+        for(auto i = 0; i < size; ++i)
+        {
+            to[i] = native_cast(from[i], tag<Managed>());
+        }
+    }
 }
 }

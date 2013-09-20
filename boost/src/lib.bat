4aed0019cd6a748b54de4f4991757796645269ae
Sergey Shandar <serge.shandar@gmail.com>
2013-09-20 09:39:06 -0700

    source based nuget packages.

--- boost/src/lib.bat
+++ boost/src/lib.bat
@@ -3,5 +3,13 @@ setlocal
 set PATH=%PATH%;C:\programs
 
 nuget.exe pack boost_atomic_src.nuspec
+nuget.exe pack boost_chrono_src.nuspec
+nuget.exe pack boost_system_src.nuspec
+nuget.exe pack boost_coroutine_src.nuspec
+nuget.exe pack boost_date_time_src.nuspec
+nuget.exe pack boost_exception_src.nuspec
+
+nuget.exe pack boost_python.nuspec
+nuget.exe pack boost_mpi.nuspec
 
 endlocal

630f38cb5f4ed721e39e7acd0dc7aed2dba24a41
Sergey Shandar <serge.shandar@gmail.com>
2013-09-30 21:27:29 -0700

    Builder.

---
+++ boost/builder/builder/CompilationUnit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace builder
+{
+    sealed class CompilationUnit
+    {
+        public readonly string Name;
+
+        public readonly IEnumerable<CppFile> FileList;
+
+        public CompilationUnit(
+            string name, IEnumerable<CppFile> fileList = null)
+        {
+            Name = name;
+            FileList = fileList.NewIfNull();
+        }
+
+        public CompilationUnit(): this(null)
+        {
+        }
+
+        public string FileName(Library library)
+        {
+            return
+                library.LibraryId + 
+                (Name == null ? "" : "_" + Name) +
+                ".cpp";
+        }
+
+        public void Make(Library library)
+        {
+            File.WriteAllLines(
+                FileName(library),
+                Collections.
+                    New(new CppFile(@"boost\config.hpp")).
+                    Concat(FileList).
+                    SelectMany(f => f.Code));
+        }
+    }
+}
---
+++ boost/builder/builder/CppFile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builder
+{
+    sealed class CppFile
+    {
+        public readonly string Name;
+
+        public readonly string Condition;
+
+        public CppFile(string name, string condition = null)
+        {
+            Name = name;
+            Condition = condition;
+        }
+
+        public IEnumerable<string> Code
+        {
+            get
+            {
+                var include = "#include \"" + Name + "\"";
+                if (Condition == null)
+                {
+                    yield return include;
+                }
+                else
+                {
+                    yield return "#if " + Condition;
+                    yield return include;
+                    yield return "#endif";
+                }
+            }
+        }
+    }
+}

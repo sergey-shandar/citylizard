7c1c215f3da91d68d3e18f53ba7cc6779aea0866
Sergey Shandar <serge.shandar@gmail.com>
2013-10-03 00:32:52 -0700

    regex

---
+++ boost/test3/test_mpi/test_mpi.cpp
@@ -0,0 +1,12 @@
+// test_mpi.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#pragma comment(lib, "i386/msmpi.lib")
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+

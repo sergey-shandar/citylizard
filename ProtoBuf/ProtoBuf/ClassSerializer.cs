35ac1fc25825da7332daaf4e49dd459241494d75
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2013-11-02 11:28:03 -0700

    - ProtoBuf

---
+++ ProtoBuf/ProtoBuf/ClassSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProtoBuf
+{
+    class ClassSerializer<T>: ISerializer<T>
+        where T: new()
+    {
+        public ClassSerializer()
+        {
+            var type = typeof(T);
+            foreach (var field in type.GetFields().Where(f => !f.IsStatic))
+            {
+                var fieldType = field.FieldType;
+            }
+        }
+
+        public void Serialize(T value, Stream stream)
+        {
+        }
+
+        public T Deserialize(Stream stream)
+        {
+            var value = new T();
+            return value;
+        }
+    }
+}

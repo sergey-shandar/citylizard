045f7ca2f00afef7f63ca48d6ab97b615c31ce35
Sergey Shandar <serge.shandar@gmail.com>
2013-11-20 19:08:23 -0800

    Base128 & ZigZag

--- csharp/CityLizard.Core/AssemblyInfo.cs
+++ csharp/CityLizard.Core/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection; 
 [assembly: AssemblyCompany("CityLizard")] 
 [assembly: AssemblyCopyright("Copyright © CityLizard 2013")] 
-[assembly: AssemblyVersion("2.2.0.0")] 
-[assembly: AssemblyFileVersion("2.2.0.0")] 
+[assembly: AssemblyVersion("2.2.1.0")] 
+[assembly: AssemblyFileVersion("2.2.1.0")] 
 [assembly: AssemblyTitle("CityLizard.Core")] 
 [assembly: AssemblyProduct("CityLizard.Core")] 

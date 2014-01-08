6e1eb89626af546a3c463134a57e1fed588eb2e9
Sergey Shandar <serge.shandar@gmail.com>
2014-01-07 18:42:30 -0800

    ObjectMap (WiP)

---
+++ csharp/CityLizard.Core/ObjectMap.BaseType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CityLizard.Core;
+
+namespace CityLizard.Core.ObjectMap
+{
+    class BaseType
+    {
+        public readonly TypeCategory Category;
+
+        protected BaseType(TypeCategory category)
+        {
+            Category = category;
+        }
+    }
+}
---
+++ csharp/CityLizard.Core/ObjectMap.RecordType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CityLizard.Core.ObjectMap
+{
+    sealed class RecordType: BaseType
+    {
+        public readonly String Name;
+        public readonly Field[] FieldList;
+ 
+        public RecordType(bool isRef, String name, Field[] fieldList):
+            base(isRef ? TypeCategory.RECORD_REF: TypeCategory.RECORD)
+        {
+            Name = name;
+            FieldList = fieldList;
+        }
+    }
+}
